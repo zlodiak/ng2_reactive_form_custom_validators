db62b76aff487fab20b6aec60d77fa45bb167e04
zlodiak <prozaik81-2@yandex.ru>
2018-11-04 23:20:37 +0300

    form formed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+	form: FormGroup;
+
+	ngOnInit() {
+    this.form = new FormGroup({
+      'year':            			new FormControl(1980, [ 
+                                                      Validators.required, 
+                                                      Validators.minLength(4), 
+                                                      Validators.maxLength(4),
+                                                      Validators.min(1900),
+                                                      Validators.pattern("^[0-9]*$")
+                                                    ]),
+  		'floor':                new FormControl(1, [
+                                                      Validators.required,
+                                                      Validators.min(1),
+                                                      Validators.max(50),
+                                                      Validators.pattern("^[0-9]*$")
+                                                    ]),
+  		'floorsCount':          new FormControl(5, [
+                                                      Validators.required,
+                                                      Validators.min(1),
+                                                      Validators.max(50),
+                                                      Validators.pattern("^[0-9]*$")
+                                                    ]),
+  		'radius':               new FormControl(1000, [
+                                                      Validators.required,
+                                                      Validators.min(1),
+                                                      Validators.pattern("^[0-9]*$")
+                                                    ])
+		})
+	}
+
+	submit() {
+		console.log('submit');
+	}
 }
